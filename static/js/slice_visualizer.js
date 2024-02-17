bf1ab222318020ddd72e7814ab390092b5e0f5bb
Daniel Kendrick <daniel_kendrick@cable.comcast.com>
2024-02-17 08:11:34 -0700

    added slice notation

--- static/js/slice_visualizer.js
+++ static/js/slice_visualizer.js
@@ -15,31 +15,31 @@ let targetCap = elements.length;
 
 const operations = {
     'Full Array': {
-        description: 'Displays the full array (slice[:]). No slicing applied, showing the entire array.',
+        description: 'Displays the full array (slice[:]). No slicing applied, showing the entire array.\n',
         start: 0,
         end: elements.length,
         cap: elements.length
     },
     'First Half': {
-        description: 'Displays the first half of the array (slice[:6]). This operation slices the array from the start until the 6th element.',
+        description: 'Displays the first half of the array (slice[:6]). This operation slices the array from the start until the 6th element.\n',
         start: 0,
         end: 6,
         cap: elements.length
     },
     'Second Half': {
-        description: 'Displays the second half of the array (slice[6:]). This slices the array from the 6th element to the end.',
+        description: 'Displays the second half of the array (slice[6:]). This slices the array from the 6th element to the end.\n',
         start: 6,
         end: elements.length,
         cap: elements.length
     },
     'Middle': {
-        description: 'Displays a middle segment of the array (slice[3:8]). This slices the array to include elements from the 3rd to the 8th.',
+        description: 'Displays a middle segment of the array (slice[3:8]). This slices the array to include elements from the 3rd to the 8th.\n',
         start: 3,
         end: 8,
         cap: elements.length
     },
     'Custom Cap': {
-        description: 'Displays a slice with a custom capacity (slice[3:6:10]). This operation demonstrates how slices can be limited in size, indicating the maximum capacity without reallocating.',
+        description: 'Displays a slice with a custom capacity (slice[3:6:10]). This operation demonstrates how slices can be limited in size, indicating the maximum capacity without reallocating.\n',
         start: 3,
         end: 6,
         cap: 10
